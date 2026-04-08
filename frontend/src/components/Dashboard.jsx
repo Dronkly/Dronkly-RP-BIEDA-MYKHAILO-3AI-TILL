32b4ee8ee865fb431fae8d21627badb7935df75b
Mykhailo Bieda <vinervin76@gmail.com>
2026-04-08 21:09:20 +0200

    Stylovani stranky pro prihlaseni+obrazek na pozadi

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Vítej v administraci EkoModa.</p>
+    <div className="dashboard-page">
+        <div className="dashboard-box">
+         <h1>Dashboard</h1>
+         <p>Vítej v administraci EkoModa.</p>
     </div>
+   </div>
   );
 };
 
